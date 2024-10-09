2ae65256a705ce087ec6886996cdc5386d80a744
michaelmugeni <michaelmugeni98@gmail.com>
2024-10-09 12:02:44 +0000

    6-set.js

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+function setFromArray(arr) {
+    return new Set(arr);
+}
+
+// Example usage:
+const array = [12, 32, 15, 78, 98, 15];
+const uniqueSet = setFromArray(array);
+console.log(setFromArray([12, 32, 15, 78, 98, 15]));
+
