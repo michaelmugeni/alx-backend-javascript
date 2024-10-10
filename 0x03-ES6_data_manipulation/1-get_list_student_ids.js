ee02374c069cab358d0dba6a3efb938244b9b3b0
Michael <136024661+michaelmugeni@users.noreply.github.com>
2024-10-10 17:44:44 +0300

    Update 1-get_list_student_ids.js

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,15 +1,6 @@
-function getListStudentIds(students) {
-    if (!Array.isArray(students)) {
-        return [];
-    }
-    return students.map(student => student.id);
+export default function getListStudentIds(students) {
+  if (students instanceof Array) {
+    return students.map((student) => student.id);
+  }
+  return [];
 }
-
-const students = [
-    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' }
-];
-
-console.log(getListStudentIds(students)); // Output: [1, 2, 5]
-
