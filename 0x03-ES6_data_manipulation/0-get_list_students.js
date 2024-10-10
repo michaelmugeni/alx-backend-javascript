8ca4277caa57ca2c331ae819ab073a1a1b07e504
Michael <136024661+michaelmugeni@users.noreply.github.com>
2024-10-10 17:41:51 +0300

    Update 0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,12 @@
-function getListStudents( ) {
-    return [
-        { id: 1, firstName: 'Guillaume' , location: 'San Francisco' },
-        { id: 2, firstName: 'James' , location: 'Columbia' },
-        { id: 5, firstName: 'Serena' , location: 'San Francisco' }
-    ]
-};
-console.log(getListStudents( ));
+/**
+ * Retrieves a list of students.
+ * @author michael mugeni <https://github.com/michaelmugeni>
+ * @returns {{id: Number, firstName: String, location: String}[]}
+ */
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+}
