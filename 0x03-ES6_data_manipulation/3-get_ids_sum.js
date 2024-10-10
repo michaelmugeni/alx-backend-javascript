a8e07f2c028adc470365aca1536349ff6f1207e2
Michael <136024661+michaelmugeni@users.noreply.github.com>
2024-10-10 17:48:20 +0300

    Update 3-get_ids_sum.js

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,16 +1,10 @@
-function getListStudents( ) {
-    return [
-        { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-        { id: 2, firstName: 'Bob', location: 'Columbia' },
-        { id: 5, firstName: 'Charlie', location: 'San Francisco' },
-    ];
+export default function getStudentIdsSum(students) {
+  if (students instanceof Array) {
+    return students.reduce(
+      (prevStudent, curStudent) => prevStudent.id || prevStudent + curStudent.id,
+      0,
+    );
+  }
+  return 0;
 }
-
-function getStudentIdsSum(students) {
-    return students.reduce((sum, student) => sum + student.id, 0);
-}
-const students = getListStudents();
-const value = getStudentIdsSum(students);
-
-console.log(value);
 
